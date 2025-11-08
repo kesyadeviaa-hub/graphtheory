2d893ed2d48a99e2b269edaa9583625529a3b018
Kesya Devia <kesyadeviaa@gmail.com>
2025-11-08 19:44:50 +0700

    Wrap profil.py code into main() function

--- profil.py
+++ profil.py
@@ -1,31 +1,29 @@
 import streamlit as st
 
-# Judul halaman
-st.title("Team Profile")
+def main():
+    st.title("Team Profile")
 
-# Data anggota tim
-team_members = [
-    {
-        "name": "Kesya Devia Anandita",
-        "major": "Actuarial Science",
-        "foto": "https://raw.githubusercontent.com/kesyadeviaa-hub/teamphoto/refs/heads/main/ACS_25102523320.jpg"
-    },
-    {
-        "name": "Arganta Mahadana Wibowo",
-        "major": "Actuarial Science",
-        "foto": "https://raw.githubusercontent.com/kesyadeviaa-hub/teamphoto/refs/heads/main/ACS_25102553036.jpg"
-    },
-    {
-        "name": "Alfazri Nurahman",
-        "major": "Actuarial Science",
-        "foto": "https://raw.githubusercontent.com/kesyadeviaa-hub/teamphoto/refs/heads/main/ACS_25102540760.jpg"
-    },
-]
+    team_members = [
+        {
+            "name": "Kesya Devia Anandita",
+            "major": "Actuarial Science",
+            "foto": "https://raw.githubusercontent.com/kesyadeviaa-hub/teamphoto/refs/heads/main/ACS_25102523320.jpg"
+        },
+        {
+            "name": "Arganta Mahadana Wibowo",
+            "major": "Actuarial Science",
+            "foto": "https://raw.githubusercontent.com/kesyadeviaa-hub/teamphoto/refs/heads/main/ACS_25102553036.jpg"
+        },
+        {
+            "name": "Alfazri Nurahman",
+            "major": "Actuarial Science",
+            "foto": "https://raw.githubusercontent.com/kesyadeviaa-hub/teamphoto/refs/heads/main/ACS_25102540760.jpg"
+        },
+    ]
 
-# Tampilkan profil dalam 3 kolom
-cols = st.columns(3)
-for i, member in enumerate(team_members):
-    with cols[i % 3]:
-        st.image(member["foto"], width=180)
-        st.markdown(f"**{member['name']}**")
-        st.caption(member["major"])
+    cols = st.columns(3)
+    for i, member in enumerate(team_members):
+        with cols[i % 3]:
+            st.image(member["foto"], width=180)
+            st.markdown(f"**{member['name']}**")
+            st.caption(member["major"])
